a8e1cea7276346a4435ed66d18cf49ab5774cfae
Jawad91b <jawadboumehdi20@gmail.com>
2025-10-07 16:51:38 +0200

    Add files via upload

---
+++ tp10/script.js
@@ -0,0 +1,65 @@
+function somme()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var c = Number(a) + Number(b); 
+    document.getElementById("resultat").value = c;
+}
+
+function soustraction()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var c = Number(a) - Number(b); 
+    document.getElementById("resultat").value = c;
+}
+function multiplication()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var c = Number(a) * Number(b); 
+    document.getElementById("resultat").value = c;
+}
+function division()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var c = Number(a) / Number(b); 
+    document.getElementById("resultat").value = c;
+}
+function addition()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var c = Number(a) +  Number(b); 
+    document.getElementById("resultat").value = c;
+}
+function pair()
+{
+
+	var x = document.getElementById("n1").value
+	if (x % 2 ==0){
+    	document.getElementById("pair").value = " Paire "
+	}
+	else{
+    	document.getElementById("pair").value = " Impaire "
+	}
+}
+function permute()
+{
+    var a = document.getElementById ("n1" ).value;
+    var b = document.getElementById ("n2").value;
+    var x = document.getElementById ("n1").value
+    var a = document.getElementById("n2").value;
+
+    document.getElementById ("n1").value = a;
+    document.getElementById ("n2").value= x ;
+}
+
+function reset()
+{
+     document.getElementById ("n1" ).value ="";
+     document.getElementById ("n2").value ="";
+     document.getElementById("pair").value ="";
+     document.getElementById("resultat").value ="";
+}
